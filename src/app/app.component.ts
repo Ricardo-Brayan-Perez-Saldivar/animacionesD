c55ec5c29e19bf719d6cccae37eabae2966892c0
revistatese <revistatese@gmail.com>
2020-04-20 14:09:42 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+import data from './data';
+
+//animaciones
+import {trigger, style,transition,animate, state} from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('enterState',[
+      state('void',style({
+        transform: 'translateX(-100%)',
+        opacity:0
+      })),
+      transition(':enter',[
+        animate(300,style({
+          transform: 'translateX(0)',
+          opacity:1
+        }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'animaciones';
+  public courses: any[] = data;
 }
---
+++ src/app/data.ts
@@ -0,0 +1,7 @@
+const data = [
+    {title:'Desarrollo WEB'},
+    {title:'Ejemplo 1'},
+    {title:'Ejemplo 2'}
+];
+
+export default data;
